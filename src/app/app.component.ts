fcce8a37cc01cc1d4d1fc8fd30cc4d712cae0017
Manuel <manne0081@gmail.com>
2022-03-16 23:49:44 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+      selector: 'app-root',
+      templateUrl: './app.component.html',
+      styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'grundstruktur';
+
+export class AppComponent implements OnInit {
+    title = 'grundstruktur';
+
+    id: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    shortName: string = "";
+
+    ngOnInit() {
+        console.log("onInit");
+    }
+
+    onSubmit(): void {
+        if(this.firstName != "" && this.lastName != "") {
+            var str;
+
+            str = this.lastName.slice(0,3);
+            str += this.firstName.slice(0,2);
+            str += "01";
+
+            this.shortName = str.toLocaleLowerCase();
+            this.id = "2393";
+        }
+    }
+
 }
 
